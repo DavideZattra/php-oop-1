02aeaa9baf59b477e78810f9f21da01ce0f333ca
DavideZattra <d.zattra@gmail.com>
2021-10-27 15:17:31 +0200

    initialized and created class

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+class Movie{
+
+    public $id;
+    public $title;
+    public $genre;
+    public $overview;
+    public $vote;
+    public $original_language;
+    public $release_date;
+
+}
